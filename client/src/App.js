d7b258b8ad7a4f872c1479a2fab7f08ada739e55
Aaron Tenn <aaron.will.tenn@gmail.com>
2021-11-19 10:10:55 -0500

    added chart

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import { Routes, Route, Link } from "react-router-dom";
 import './App.scss';
 import axios from 'axios';
+import React from 'react';
+import {Line} from 'react-chartjs-2'
 
 function App() {
   // const options = {
   //   method: 'GET',
-  //   url: 'https://stock-data-yahoo-finance-alternative.p.rapidapi.com/v6/finance/quote',
-  //   params: {symbols: 'AAPL,ETH-USD'},
+  //   url: 'https://stock-data-yahoo-finance-alternative.p.rapidapi.com/v8/finance/chart/AAPL',
   //   headers: {
   //     'x-rapidapi-host': 'stock-data-yahoo-finance-alternative.p.rapidapi.com',
   //     'x-rapidapi-key': 'rw9oV5YAcGmshkCGpJdkhwRAXbnAp1HofApjsntB8od230Yqct'
@@ -19,6 +20,7 @@ function App() {
   // }).catch(function (error) {
   //   console.error(error);
   // });
+
   return (
     <div className="App">
         <h1>Welcome to React Router!</h1>
@@ -31,8 +33,39 @@ function App() {
 }
 
 function Home() {
+  const state = {
+    labels: ['January', 'February', 'March',
+             'April', 'May'],
+    datasets: [
+      {
+        label: 'Rainfall',
+        fill: false,
+        lineTension: 0.5,
+        backgroundColor: 'rgba(75,192,192,1)',
+        borderColor: 'rgba(0,0,0,1)',
+        borderWidth: 2,
+        data: [65, 59, 80, 81, 56]
+      }
+    ]
+  }
   return (
     <>
+    <div style={{ position: "relative", margin: "auto", width: "60vw" }}>
+      <Line
+          data={state}
+          options={{
+            title:{
+              display:true,
+              text:'Average Rainfall per month',
+              fontSize:20
+            },
+            legend:{
+              display:true,
+              position:'right'
+            }
+          }}
+        />
+    </div>
       <main>
         <h2>Welcome to the homepage!</h2>
         <p>You can do this, I believe in you.</p>
@@ -40,11 +73,13 @@ function Home() {
       <nav>
         <Link to="/about">About</Link>
       </nav>
+      
     </>
   );
 }
 
 function About() {
+
   return (
     <>
       <main>
