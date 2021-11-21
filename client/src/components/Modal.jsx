16a657091651a801189bc2af3f2817488f0e9f4d
Adriana Calvo-Matos <adriana.calvomatos@gmail.com>
2021-11-21 10:00:52 -0500

    setup modal

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -2,7 +2,19 @@
 // needs the stock name
 
 const Model = (props) => {
-  return <p>Model Buy/Sell</p>;
+  return (
+    <aside>
+      <div className='modal-background'>
+        <div className='modal-container'>
+          {/* <button onClick={() => setModalOpen(false)}> X</button> */}
+
+          <h1>Model Buy/Sell</h1>
+
+          <form action=''></form>
+        </div>
+      </div>
+    </aside>
+  );
 };
 
 export default Model;
