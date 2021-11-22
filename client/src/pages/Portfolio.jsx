81bff6543537ceb8da65b6c2621e427a3c7c0548
niubrandon <niubrandon@gmail.com>
2021-11-21 22:39:25 -0500

    portfolio dougnut chart with hardcoded data

--- client/src/pages/Portfolio.jsx
+++ client/src/pages/Portfolio.jsx
@@ -1,6 +1,9 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import Table from 'react-bootstrap/Table';
+import Card from 'react-bootstrap/Card';
+import Button from "react-bootstrap/Button";
+import PortfolioDonutChart from "../components/PortfolioDonutChart"; 
 
 const Portfolio = (props) => {
 
@@ -24,13 +27,30 @@ const Portfolio = (props) => {
     })
 
   }, [])
+
+  const portfolio_cards = account && account.portfolio.map((item, index) => {
+    return (
+    <Card key={index} className="w-50 text-center" >
+      <Card.Header as="h5">{item.ticker}</Card.Header>
+      <Card.Body>
+        <Card.Title>quantity: {item.quantity}</Card.Title>
+        <Card.Text>
+          {`Current market value is ${item.quantity * item.current_spot_price} , representing  ${Number(item.quantity * item.current_spot_price / account.account.total_balance * 100).toFixed(2)}% of your total portfolio`}
+        </Card.Text>
+        <Button variant="primary">Trade</Button>
+      </Card.Body>
+    </Card>
+    )
+
+  })
+
   const tableRows = account && account.transactions.map((item, index) => {
     return (
       <tr key = {index} >
       <td>{index + 1}</td>
       <td>{item.ticker}</td>
       <td>{item.quantity}</td>
-      <td>{item.trade}</td>
+      <td>{item.trade === 1 ? "buy" : "sell"}</td>
       <td>{item.settled_price}</td>
       <td>{item.quantity * item.settled_price}</td>
     </tr>
@@ -39,13 +59,13 @@ const Portfolio = (props) => {
   const TransactionsTable = () => {
     return (
 
-         <Table responsive="sm" hover>
+         <Table responsive="sm" className="w-50" hover>
           <thead>
             <tr>
               <th>#</th>
               <th>Name</th>
               <th>Quantity</th>
-              <th>Buy or sell</th>
+              <th>Trade</th>
               <th>Settled price</th>
               <th>Total</th>
             </tr>
@@ -58,8 +78,13 @@ const Portfolio = (props) => {
     )
   }
 
+
   return (
+    <>
+    { account && <PortfolioDonutChart />}
+    {account && portfolio_cards}
     <TransactionsTable />
+    </>
   )
 }
 
