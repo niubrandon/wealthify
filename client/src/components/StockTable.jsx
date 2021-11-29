a101b11c3901082d6500fdc0c47299807ef09cf6
niubrandon <niubrandon@gmail.com>
2021-11-28 22:17:31 -0500

    stock data table undefined issue due to state chasing from two useeffects

--- client/src/components/StockTable.jsx
+++ client/src/components/StockTable.jsx
@@ -13,53 +13,57 @@ const StockTable = (props) => {
     regularMarketOpen,
   } = props;
   return (
-    <article id='stock-table'>
-      <h2 className='title'>Key Performance Indicators</h2>
-      <table className='info'>
-        <tbody>
-        <tr>
-          <td>Regular Market Price</td>
-          <td>{`$${regularMarketPrice.toFixed(2)}`}</td>
-        </tr>
-        <tr>
-          <td>Regular Market Change</td>
-          <td>{`$${regularMarketChange.toFixed(2)}`}</td>
-        </tr>
-        <tr>
-          <td>Regular MarketChange Percent</td>
-          <td>{`${regularMarketChangePercent.toFixed(2)}%`}</td>
-        </tr>
-        <tr>
-          <td>Market Cap</td>
-          <td>{`${(marketCap/1000000000).toFixed(2)}B`}</td>
-        </tr>
-        <tr>
-          <td>Regular Market Day High</td>
-          <td>{`$${regularMarketDayHigh.toFixed(2)}`}</td>
-        </tr>
-        <tr>
-          <td>Regular Market Day Low</td>
-          <td>{`$${regularMarketDayLow.toFixed(2)}`}</td>
-        </tr>
-        <tr>
-          <td>Regular Market Volume</td>
-          <td>{`${(regularMarketVolume/1000000).toFixed(2)}M`}</td>
-        </tr>
-        <tr>
-          <td>Regular Market Previous Close</td>
-          <td>{`$${regularMarketPreviousClose.toFixed(2)}`}</td>
-        </tr>
-        <tr>
-          <td>Exchange</td>
-          <td>{exchange}</td>
-        </tr>
-        <tr>
-          <td>Regular Market Open</td>
-          <td>{`$${regularMarketOpen.toFixed(2)}`}</td>
-        </tr>
-        </tbody>
-      </table>
-    </article>
+    <div>
+      { props && <article id='stock-table'>
+        <h2 className='title'>Key Performance Indicators</h2>
+        <table className='info'>
+          <tbody>
+          <tr>
+            <td>Regular Market Price</td>
+            <td>{`$${regularMarketPrice?.toFixed(2)}`}</td>
+          </tr>
+          <tr>
+            <td>Regular Market Change</td>
+            <td>{`$${regularMarketChange?.toFixed(2)}`}</td>
+          </tr>
+          <tr>
+            <td>Regular MarketChange Percent</td>
+            <td>{`${regularMarketChangePercent?.toFixed(2)}%`}</td>
+          </tr>
+          <tr>
+            <td>Market Cap</td>
+            <td>{`${(marketCap/1000000000)?.toFixed(2)}B`}</td>
+          </tr>
+          <tr>
+            <td>Regular Market Day High</td>
+            <td>{`$${regularMarketDayHigh?.toFixed(2)}`}</td>
+          </tr>
+          <tr>
+            <td>Regular Market Day Low</td>
+            <td>{`$${regularMarketDayLow?.toFixed(2)}`}</td>
+          </tr>
+          <tr>
+            <td>Regular Market Volume</td>
+            <td>{`${(regularMarketVolume/1000000)?.toFixed(2)}M`}</td>
+          </tr>
+          <tr>
+            <td>Regular Market Previous Close</td>
+            <td>{`$${regularMarketPreviousClose?.toFixed(2)}`}</td>
+          </tr>
+          <tr>
+            <td>Exchange</td>
+            <td>{exchange}</td>
+          </tr>
+          <tr>
+            <td>Regular Market Open</td>
+            <td>{`$${regularMarketOpen?.toFixed(2)}`}</td>
+          </tr>
+          </tbody>
+        </table>
+      </article>}
+
+    </div>
+   
   );
 };
 
